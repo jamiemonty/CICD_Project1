6d06a508cf771a99f71a34e7c38fa2e0e1eb86b6
Jamie <G00419525@atu.ie>
2025-01-05 22:10:35 +0000

    added code to enable communication with gymgoal project using OpenFeign to get data from the gym goals MySQL table upon post request.

--- src/main/java/ie/atu/userselection/UserSelectionController.java
+++ src/main/java/ie/atu/userselection/UserSelectionController.java
@@ -3,7 +3,9 @@ package ie.atu.userselection;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -12,9 +14,11 @@ import java.util.Optional;
 public class UserSelectionController {
 
     private final UserSelectionRepository userSelectionRepository;
+    private final GoalClient goalClient;
 
-    public UserSelectionController(UserSelectionRepository userSelectionRepository) {
+    public UserSelectionController(UserSelectionRepository userSelectionRepository, GoalClient goalClient) {
         this.userSelectionRepository = userSelectionRepository;
+        this.goalClient = goalClient;
     }
 
     // Get all user selections
@@ -32,8 +36,20 @@ public class UserSelectionController {
 
     // Create a new user selection
     @PostMapping
-    public UserSelection createUserSelection(@RequestBody UserSelection userSelection) {
-        return userSelectionRepository.save(userSelection);
+    public ResponseEntity<Map<String, Object>> createUserSelection(@RequestBody UserSelection userSelection) {
+        // Save the user selection to the database
+        UserSelection savedSelection = userSelectionRepository.save(userSelection);
+
+        // Fetch GymGoal data from the external service using Feign client
+        GymGoal gymGoal = goalClient.getGymGoalById(userSelection.getGoalId());
+
+        // Prepare the response with sets and reps
+        Map<String, Object> response = new HashMap<>();
+        response.put("userSelectionId", savedSelection.getSelectId());
+        response.put("sets", gymGoal.getSets());
+        response.put("reps", gymGoal.getReps());
+
+        return ResponseEntity.ok(response);
     }
 
     // Update an existing user selection
